77db046277bdf277c823ad680ba0e28ae439da5c
Cfalz <cfalz002@ucr.edu>
2015-10-25 17:17:03 -0700

    Added Convert function to Connector class.

--- rshell/Connector.cc
+++ rshell/Connector.cc
@@ -1,16 +1,36 @@
 #include "Connector.hh"
 
+
 Connector::Connector()
-{}
-void execute(string cmd_str)
 {
-
-
+	success = false;
 }
+
+
 Connector::~Connector()
 {}
 
 
+void Connector::execute(string cmd_str)
+{
+
+
+}
+
+void Connector::convert(queue<string> command_queue, char* args[])
+{
+	int size = command_queue.size();
+	int i = 0;
+
+	while(!command_queue.empty())
+	{
+		cout <<"Front of queue is: " << command_queue.front() << endl;
+		args[i] = (char*)command_queue.front().c_str();
+		command_queue.pop();
+		i++;
+	}
+	args[i] = NULL;
+}
 
 
 
--- rshell/Connector.hh
+++ rshell/Connector.hh
@@ -1,22 +1,34 @@
 #ifndef Connector_hh
 #define Connector_hh
 
+#include <string>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
 // base class for the And, Or, ; , and : classes
 
 class Connector
 {
 
-public:
-	Connector();
-	~Connector();
-	void virtual execute(string cmd_str);
+	public:
+		Connector();
+		~Connector();
 
-	bool is_connector(char*);
+		void virtual execute(string cmd_str);
+		void convert(queue<string> command_queue, char* args[]);
+		bool is_connector(char*);
 
-private:
+		bool get_success();
+		bool set_success(bool value);
+
+	private:
+		bool success;
 
 
 };
+
 #endif
 
 
